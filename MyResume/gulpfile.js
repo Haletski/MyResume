cd06b7ce2cb50bdce9846578215c923d68aa35c5
Haletski <haletski.p@gmail.com>
2019-04-06 17:29:23 +0300

    init commit

---
+++ MyResume/gulpfile.js
@@ -0,0 +1,26 @@
+﻿/// <binding BeforeBuild='sass' AfterBuild='sass' />
+var sass = require("gulp-sass");
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+var gutil = require('gulp-util');
+
+var paths = {
+    src: "./Content/Sass/main.scss",
+    dest: "./Content/Css"
+};
+
+gulp.task('watch', function(){
+    gulp.watch('./Content/Sass/**/*.scss', ['sass']);
+})
+
+gulp.task('sass', function () {
+    return gulp.src(paths.src)
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .on('error', gutil.log)
+        .pipe(concat('styles.css'))
+        .pipe(autoprefixer({ browsers: ["last 2 versions"] }))
+        .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task('default', ['sass','watch'])
