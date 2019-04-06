35480890f09848a90bb9775133d81438046fa8c2
Haletski <haletski.p@gmail.com>
2019-04-07 00:59:48 +0300

    updated structure and gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,55 @@
-﻿/// <binding BeforeBuild='sass' AfterBuild='sass' />
-var sass = require("gulp-sass");
+﻿var sass = require("gulp-sass");
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
 var gutil = require('gulp-util');
+var inject = require('gulp-inject');
+var uglify = require('gulp-uglify');
+var clean = require('gulp-clean');
 
 var paths = {
-    src: "./Content/Sass/main.scss",
-    dest: "./Content/Css"
+    src: "./Content/src",
+    dest: "./Content/dist"
 };
 
+gulp.task('inject', function(){
+    var target = gulp.src('./index.html');
+    var sources = gulp.src([ paths.dest + '/Css/**/*.css', paths.dest + '/Scripts/**/*.js'], { read: false });
+
+    return target.pipe(inject(sources, {   
+        relative: true
+    }))
+    .pipe(gulp.dest('./'));
+});
+
 gulp.task('watch', function(){
-    gulp.watch('./Content/Sass/**/*.scss', ['sass']);
+    return gulp.watch( paths.src +'/Sass/**/*.scss', ['sass']);
 })
 
+gulp.task('assets', function(){
+   return gulp.src(paths.src + '/Assets/**/*.*')
+    .pipe(gulp.dest(paths.dest + '/Assets'))
+});
+
+gulp.task('scripts', function(){
+   return gulp.src(paths.src + '/Scripts/**/*.js')
+    .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dest + '/Scripts'))
+});
+
 gulp.task('sass', function () {
-    return gulp.src(paths.src)
+    return gulp.src(paths.src + '/Sass/main.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
         .on('error', gutil.log)
         .pipe(concat('styles.css'))
         .pipe(autoprefixer({ browsers: ["last 2 versions"] }))
-        .pipe(gulp.dest(paths.dest));
+        .pipe(gulp.dest(paths.dest + '/Css'));
 });
 
-gulp.task('default', ['sass','watch'])
+gulp.task('clean', function(){
+    return gulp.src(paths.dest + '/**/*', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('default', ['clean', 'sass', 'scripts', 'assets', 'inject', 'watch'])
